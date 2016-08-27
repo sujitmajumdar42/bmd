934ba93241ac6eee3b7fd95a8855db9a79fb0bda
Sujit Majumdar <sujitmajudar2@gmail.com>
2016-08-27 10:09:23 +0530

    View folder with node interaction.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-/*var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});*/
-var http = require('http')
-console.log(process.env.PORT);
+/*var http = require('http')
 http.createServer(function(request,response){
 	response.writeHead(200,{"Content-Type":"text/plain"})
 	response.end("Book My dine at "+process.env.PORT+"\n database:"+process.env.DATABASE_URL)
-	console.log(process.env.PORT);
-}).listen(process.env.PORT)
+	
+}).listen(process.env.PORT,function(){
+	console.log("App is running at:"+process.env.PORT);
+})*/
+
+var express = require('express')
+  , bodyParser = require("body-parser")
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+module.exports = app;
+
+var nconf = require('nconf');
+nconf.argv().env().file({ file: 'config.json' });
+
+app.set('port', process.env.PORT || nconf.get('node:port'));
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+app.use('/', router);
+app.get('/', function(req, res) {
+    res.sendfile('./views/index.html');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Server listening on port ' + app.get('port'));
+});
